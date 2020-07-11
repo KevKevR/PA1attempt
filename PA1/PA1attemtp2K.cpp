f35b32110569d6e960b9c89a4cbe9cba7e34e00a
Zoomzeta <68072280+Zoomzeta@users.noreply.github.com>
2020-07-11 17:16:11 -0400

    Update PA1attemtp2K.cpp

--- PA1/PA1attemtp2K.cpp
+++ PA1/PA1attemtp2K.cpp
@@ -1156,8 +1156,8 @@ mat4* modelControl(GLFWwindow* window) {
     map<int, transformation>::iterator itr;
     float transformSpeed = 0.2f;
     float translateSpeed = transformSpeed;
-    float rotateSpeed = 5.0f;   //specifications
-    float scaleSpeed = transformSpeed / 4;
+    float rotateSpeed = 0.0873f;   //specifications
+    float scaleSpeed = transformSpeed / 20;
     if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) // capital case letters
     {
         //translate model if pressed
