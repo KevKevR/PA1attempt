dc1521323aca2c9cae9ccd60c8dbcc9a29e1b77f
KevKevR <60107459+KevKevR@users.noreply.github.com>
2020-07-12 15:02:34 -0400

    Update PA1attemtp2K.cpp
    changed OpenGL version to 3.1 and onwards.

--- PA1/PA1attemtp2K.cpp
+++ PA1/PA1attemtp2K.cpp
@@ -1268,8 +1268,8 @@ int main(int argc, char* argv[])
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #else
-    // On windows, we set OpenGL version to 2.1, to support more hardware
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    // On windows, we set OpenGL version to 3.1.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 #endif
 
